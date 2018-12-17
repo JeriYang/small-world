127c6c44103de729b3d5cdc996ad9e3f4d70fe30
JeriYang <45070576+JeriYang@users.noreply.github.com>
2018-12-17 19:48:55 +0800

    Update SmallWorld.h
    add Chinese note

--- SmallWorld/SmallWorld/SmallWorld.h
+++ SmallWorld/SmallWorld/SmallWorld.h
@@ -10,54 +10,54 @@
 using namespace std;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
-class Edge
+class Edge  //边
 {
 public:
 
-	//constructors
+	//constructors 构造函数
 	Edge(string, string, string, int, int, bool);
 	Edge();
 
-	//destructor
+	//destructor 析构函数（用于销毁对象）
 	~Edge();
 
-	//operator overloading
+	//operator overloading 运算符重载，定义该对象的小于和等于
 	bool operator < (const Edge& e) const;
 	Edge& operator = (const Edge& e);
 
-	//members
-	string  from;
-	string to;
-	int w;
-	int vw;
-	string value;
-	bool is_visited;
+	//members 元素
+	string  from;  //起始地
+	string to;  //目的地
+	int w;      //
+	int vw;     //
+	string value;  //字符串的值(名称)
+	bool is_visited; //是否被访问
 
 
 };
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
-class Graph
+class Graph  //关系图
 {
 
 private:
-	void readData();    //read data from files
-	void putData(string, vector<string>);    //put data in the graph DS
-	pair<int, int> Dijkstra(string, string, int);    //Dijkstra shortest path algorithm
+	void readData();    //read data from files  从文件中读取数据
+	void putData(string, vector<string>);    //put data in the graph DS  将数据放入图表DS中
+	pair<int, int> Dijkstra(string, string, int);    //Dijkstra shortest path algorithm (Dijkstra最短路径算法)
 
 public:
-	Graph();    //constructor
-	~Graph();    //destructor
+	Graph();    //constructor 构造函数
+	~Graph();    //destructor 析构函数
 
 	void bounce();    //get distribution of degree of seberation related to on actor/actress
-	void degOfSeperation(string, string, int);    //get degree of seperation and relation strength between two actors/actress
-	void queries(int);    //read queries from file
-	void printChain(string, string);    //print chain of movies between two actors/actress
+	void degOfSeperation(string, string, int);    //get degree of seperation and relation strength between two actors/actress 获得两个演员/女演员之间的分离程度和关系强度
+	void queries(int);    //read queries from file  从文件中读取查询
+	void printChain(string, string);    //print chain of movies between two actors/actress  在两位演员/女演员之间打印电影链
 
-	//members
-	map<string, vector<Edge> > adjList;    //graph DS   
-	map<string, vector<Edge> > temp;    //temp adjlist
+	//members  元素
+	map<string, vector<Edge> > adjList;    //graph DS DS图表
+	map<string, vector<Edge> > temp;    //temp adjlist 
 	map<string, pair<int, int> > dist;    //map store degree of seperation and relation strength between two actors/actress
 	map<string, pair<string, string> > prev;    //map store chain of movies between two actors/actress
 
